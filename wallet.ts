2646400b5ff96c05a6d1f68e5a4ee215b35c6fa2
0xabin <abinbvinod@gmail.com>
2024-05-17 16:54:40 +0530

    Blance

--- wallet.ts
+++ wallet.ts
@@ -2,13 +2,29 @@ import { createWalletClient, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { arbitrumSepolia } from "@alchemy/aa-core";
 import { WalletClientSigner } from "@alchemy/aa-core";
+import { config } from 'dotenv';
+config();
 
 
-const connect = "connect wallet"
-console.log(connect);
-
-
-
+// const connect = "connect wallet"
+// console.log(connect);
 const account = privateKeyToAccount(`0x${process.env.PRIVATE_KEY}`);
-
 console.log(account);
+
+
+export const client = createWalletClient({
+    account,
+    chain: arbitrumSepolia,
+    transport: http(),
+  });
+
+//   console.log(client)
+
+
+
+export const eoaSigner = new WalletClientSigner(
+ client,
+"local"
+);
+
+// console.log(eoaSigner)
